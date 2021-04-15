5773dc5d6b21a803343fd172f4343716baa1a794
Jemaica Mae Mon <201810138@gordoncollege.edu.ph>
2021-04-15 00:12:52 -0700

    Create Place Order API for Backend

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,24 @@ export const generateToken = (user) => {
 		}
 	);
 };
+
+export const isAuth = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (authorization) {
+		const token = authorization.slice(7, authorization.length); // get rid of bearer and return token
+		jwt.verify(
+			token,
+			process.env.JWT_SECRET || '@Coder_Secret_K3y',
+			(err, decode) => {
+				if (err) {
+					req.status(401).send({ message: 'Invalid Token' });
+				} else {
+					req.user = decode;
+					next();
+				}
+			}
+		);
+	} else {
+		req.status(401).send({ message: 'No Token' });
+	}
+};
