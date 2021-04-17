d6c228305c142128c3d1f12d26fc9f1ab76a2307
Jemaica Mae Mon <201810138@gordoncollege.edu.ph>
2021-04-17 03:56:35 -0700

    Create List Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import OrderListScreen from './screens/OrderListScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
@@ -98,6 +99,10 @@ function App() {
 					<AdminRoute
 						path="/productlist"
 						component={ProductListScreen}
+					></AdminRoute>
+					<AdminRoute
+						path="/orderlist"
+						component={OrderListScreen}
 					></AdminRoute>
 					<PrivateRoute
 						path="/profile"
